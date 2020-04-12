16f942910d5d5d26753b03633a2d9fce03391647
Janusz Wdówka <j.wdowka@zsp9.elodz.edu.pl>
2020-04-12 21:41:00 +0200

    1_6TypyGeneryczne

--- CSharpStrukturyGeneryczne/1_TypyGeneryczne/Program.cs
+++ CSharpStrukturyGeneryczne/1_TypyGeneryczne/Program.cs
@@ -6,23 +6,46 @@ namespace _1_TypyGeneryczne
     {
         static void Main(string[] args)
         {
-            var kolejka = new KolejkaKolowa(3);
-            while (true)
+            var kolejka = new KolejkaKolowa<double>(3);
+            var kolejkaInt = new KolejkaKolowa<int>();
+            var kolejkaString = new KolejkaKolowa<string>(1000);
+
+            var kolejkaOsob = new KolejkaKolowa<Osoba>();
+            kolejkaOsob.Zapisz(new Osoba { Imie = "Marcin", Nazwisko = "Nowak" });
+            kolejkaOsob.Zapisz(new Osoba { Imie = "Tomek", Nazwisko = "Nowak" });
+            kolejkaOsob.Zapisz(new Osoba { Imie = "Jacek", Nazwisko = "Nowak" });
+            kolejkaOsob.Zapisz(new Osoba { Imie = "Marcin", Nazwisko = "Nowak" });
+
+            while (!kolejkaOsob.JestPusty)
             {
-                var wartosc = 0.0;
-                var wartoscwejsciowa = Console.ReadLine();
-                if(double.TryParse(wartoscwejsciowa, out wartosc))
-                {
-                    kolejka.Zapisz(wartosc);
-                    continue;
-                }
-                break;
+                var wynik = kolejkaOsob.Czytaj().Imie;
+                Console.WriteLine(wynik);
             }
-            Console.WriteLine("W naszej kolejce jest: ");
-            while (!kolejka.JestPusty)
-            {
-                Console.WriteLine("\t\t" + kolejka.Czytaj());
-            }
+
+            //while (true)
+            //{
+            //    var wartosc = 0.0;
+            //    var wartoscwejsciowa = Console.ReadLine();
+            //    if(double.TryParse(wartoscwejsciowa, out wartosc))
+            //    {
+            //        kolejka.Zapisz(wartosc);
+            //        continue;
+            //    }
+            //    break;
+            //}
+            //var suma = 0.0;
+            //Console.WriteLine("W naszej kolejce jest: ");
+            //while (!kolejka.JestPusty)
+            //{
+            //    suma += kolejka.Czytaj();
+            //}
+            //Console.WriteLine(suma);
         }
+    }
+
+    public class Osoba
+    {
+        public string Imie { get; set; }
+        public string Nazwisko { get; set; }
     }
 }
--- CSharpStrukturyGeneryczne/1_TypyGeneryczneTests/KolejkaKolowaTests.cs
+++ CSharpStrukturyGeneryczne/1_TypyGeneryczneTests/KolejkaKolowaTests.cs
@@ -10,7 +10,7 @@ namespace _1_TypyGeneryczneTests
         [TestMethod]
         public void NowaKolejkaJestPusta()
         {
-            var kolejka = new KolejkaKolowa();
+            var kolejka = new KolejkaKolowa<double>();
 
             Assert.IsTrue(kolejka.JestPusty);
         }
@@ -18,7 +18,7 @@ namespace _1_TypyGeneryczneTests
         [TestMethod]
         public void KolejkaTrzyelementowaJestPelnaPoTrzechZapisach()
         {
-            var kolejka = new KolejkaKolowa(3);
+            var kolejka = new KolejkaKolowa<double>(3);
             kolejka.Zapisz(3.8);
             kolejka.Zapisz(5.7);
             kolejka.Zapisz(9.3);
@@ -28,9 +28,9 @@ namespace _1_TypyGeneryczneTests
         [TestMethod]
         public void PierwszyWchodziPierwszyWychodzi()
         {
-            var kolejka = new KolejkaKolowa(3);
-            var wartosc1 = 4.6;
-            var wartosc2 = 3.7;
+            var kolejka = new KolejkaKolowa<string>(3);
+            var wartosc1 = "4.6";
+            var wartosc2 = "3.7";
             kolejka.Zapisz(wartosc1);
             kolejka.Zapisz(wartosc2);
             Assert.AreEqual(wartosc1, kolejka.Czytaj());
@@ -41,7 +41,7 @@ namespace _1_TypyGeneryczneTests
         [TestMethod]
         public void NadpisujeGdyJestWiekszaNizPojemnosc()
         {
-            var kolejka = new KolejkaKolowa(3);
+            var kolejka = new KolejkaKolowa<double>(3);
             var wartosci = new[] { 1.2, 2.4, 3.6, 4.8, 5.3, 6.8 };
             foreach(var wartosc in wartosci)
             {
