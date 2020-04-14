c77221ad0567d4aaa0e4ed6685e2264d6774f46a
Janusz Wdówka <j.wdowka@zsp9.elodz.edu.pl>
2020-04-14 12:28:52 +0200

    2_4Kolejka

--- CSharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
+++ CSharpStrukturyGeneryczne/2_KolekcjeGeneryczne/Program.cs
@@ -7,33 +7,18 @@ namespace _2_KolekcjeGeneryczne
     {
         static void Main(string[] args)
         {
-            //List<Pracownik> pracownicy = new List<Pracownik>
-            //{
-            //    new Pracownik{ Imie = "Marcin", Nazwisko ="Nowak" },
-            //    new Pracownik{ Imie = "Jan", Nazwisko ="Nowak" },
-            //    new Pracownik{ Imie = "Tomasz", Nazwisko ="Nowak" },
-            //};
-            //pracownicy.Add(new Pracownik { Imie = "Ania", Nazwisko = "Kos" });
-            //foreach (var pracownik in pracownicy)
-            //{
-            //    Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
-            //}
-            //for(int i = 0; i < pracownicy.Count; ++i)
-            //{
-            //    Console.WriteLine(pracownicy[i].Imie + " " + pracownicy[i].Nazwisko);
+            Queue<Pracownik> kolejka = new Queue<Pracownik>();
+            kolejka.Enqueue(new Pracownik { Imie = "Marcin", Nazwisko = "Nowak" });
+            kolejka.Enqueue(new Pracownik { Imie = "Tomek", Nazwisko = "Kowak" });
+            kolejka.Enqueue(new Pracownik { Imie = "Jacek", Nazwisko = "Zajac" });
+            kolejka.Enqueue(new Pracownik { Imie = "Ola", Nazwisko = "Kaczor" });
 
-            //}
-            var liczby = new List<int>();
-            var pojemnosc = -1;
-            while (true)
+            while (kolejka.Count > 0)
             {
-                if(liczby.Capacity != pojemnosc)
-                {
-                    pojemnosc = liczby.Capacity;
-                    Console.WriteLine(pojemnosc);
-                }
-                liczby.Add(1);
+                var pracownik = kolejka.Dequeue();
+                Console.WriteLine(pracownik.Imie + " " + pracownik.Nazwisko);
             }
+
         }
     }
 }
