1508faaa6b6c376b1e0768d0e1c74d8298639b70
Janusz Wdówka <j.wdowka@zsp9.elodz.edu.pl>
2020-04-14 12:09:19 +0200

    2_3ListaTesty

---
+++ CSharpStrukturyGeneryczne/2_KolekcjeGeneryczneTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("2_KolekcjeGeneryczneTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("2_KolekcjeGeneryczneTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f0c59733-b95a-4ac6-a325-afd23f758066")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
