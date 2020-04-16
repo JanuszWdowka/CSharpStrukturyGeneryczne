3a2be9c91a452e878627b0888134b2ffe8f1c858
Janusz Wdówka <j.wdowka@zsp9.elodz.edu.pl>
2020-04-16 12:44:21 +0200

    3_0DoCzegoSłużąInterfejsy

---
+++ CSharpStrukturyGeneryczne/3_KlasyIInterfejsyGeneryczneTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("3_KlasyIInterfejsyGeneryczneTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("3_KlasyIInterfejsyGeneryczneTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("91d45220-5a90-40b2-b1ad-a1859f2213f3")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
