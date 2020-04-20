74e17b41ecae09646bcec30cda00e0de8a704041
Janusz Wdówka <j.wdowka@zsp9.elodz.edu.pl>
2020-04-20 22:08:56 +0200

    4_1MetodyGeneryczne

---
+++ CSharpStrukturyGeneryczne/4_MetodyDelegatyGeneryczne/KolejkaKolowa.cs
@@ -0,0 +1,30 @@
+﻿namespace _4_MetodyDelegatyGeneryczne
+{
+    public class KolejkaKolowa<T> : DuzaKolejka<T>
+    {
+        private int _pojemosc;
+
+        public KolejkaKolowa(int pojemnosc = 5)
+        {
+            _pojemosc = pojemnosc;
+        }
+
+        public override void Zapisz(T wartosc)
+        {
+            base.Zapisz(wartosc);
+
+            if (kolejka.Count > _pojemosc)
+            {
+                kolejka.Dequeue();
+            }
+        }
+
+        public override bool JestPelny
+        {
+            get
+            {
+                return kolejka.Count == _pojemosc;
+            }
+        }
+    }
+}
