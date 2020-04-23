430fd9b1186f6edda45f955fec9ce1c032df3c47
Janusz Wdówka <j.wdowka@zsp9.elodz.edu.pl>
2020-04-23 23:04:08 +0200

    4_8ZdarzeniaGeneryczne

--- CSharpStrukturyGeneryczne/4_MetodyDelegatyGeneryczne/KolejkaKolowa.cs
+++ CSharpStrukturyGeneryczne/4_MetodyDelegatyGeneryczne/KolejkaKolowa.cs
@@ -1,4 +1,6 @@
-﻿namespace _4_MetodyDelegatyGeneryczne
+﻿using System;
+
+namespace _4_MetodyDelegatyGeneryczne
 {
     public class KolejkaKolowa<T> : DuzaKolejka<T>
     {
@@ -15,7 +17,17 @@
 
             if (kolejka.Count > _pojemosc)
             {
-                kolejka.Dequeue();
+                var usuniety = kolejka.Dequeue();
+                PoUsunieciuElementu(usuniety, wartosc);
+            }
+        }
+
+        private void PoUsunieciuElementu(T usuniety, T wartosc)
+        {
+            if(elementUsuniety != null)
+            {
+                var args = new ElementUsunietyEventArgs<T>(usuniety, wartosc);
+                elementUsuniety(this, args);
             }
         }
 
@@ -25,6 +37,20 @@
             {
                 return kolejka.Count == _pojemosc;
             }
+        }
+
+        public event EventHandler<ElementUsunietyEventArgs<T>> elementUsuniety;
+    }
+
+    public class ElementUsunietyEventArgs<T> : EventArgs
+    {
+        public T ElemenetUsuniety { get; set; }
+        public T ElementNowy { get; set; }
+
+        public ElementUsunietyEventArgs(T elementUsuniety, T elementNowy)
+        {
+            ElemenetUsuniety = elementUsuniety;
+            ElementNowy = elementNowy;
         }
     }
 }
--- CSharpStrukturyGeneryczne/4_MetodyDelegatyGeneryczne/Program.cs
+++ CSharpStrukturyGeneryczne/4_MetodyDelegatyGeneryczne/Program.cs
@@ -17,19 +17,27 @@ namespace _4_MetodyDelegatyGeneryczne
             //drukuj(jestMniejszeOdSto(potegowanie(dodaj(6, 8))));
 
 
-            var kolejka = new KolejkaKolowa<double>();
+            var kolejka = new KolejkaKolowa<double>(pojemnosc: 3);
+
+            kolejka.elementUsuniety += Kolejka_elementUsuniety;
+
             WprowadzanieDanych(kolejka);
 
-            var jakoData = kolejka.Mapuj(d => new DateTime(2018, 1, 1).AddDays(d));
+            //var jakoData = kolejka.Mapuj(d => new DateTime(2018, 1, 1).AddDays(d));
 
-            foreach (var item in jakoData)
-            {
-                Console.WriteLine(item);
-            }
+            //foreach (var item in jakoData)
+            //{
+            //    Console.WriteLine(item);
+            //}
 
             kolejka.Drukuj(d => Console.WriteLine(d));
 
             PrzetwarzanieDanych(kolejka);
+        }
+
+        private static void Kolejka_elementUsuniety(object sender, ElementUsunietyEventArgs<double> e)
+        {
+            Console.WriteLine("Kolejka jest pełna. Element usunięty to: {0} Nowy element to {1}", e.ElemenetUsuniety, e.ElementNowy);
         }
 
         private static void PrzetwarzanieDanych(IKolejka<double> kolejka)
